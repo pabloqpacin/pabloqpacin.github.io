4a9f251f2089bf35dbb2cd91a858f37b8e19b355
pabloqpacin <110383181+pabloqpacin@users.noreply.github.com>
2024-09-07 02:05:34 +0200

    Add new empty blog

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-import tailwind from '@astrojs/tailwind';
-import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
